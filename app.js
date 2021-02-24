c75cb7bfb329b1104b31e7fed8ede4d7c38c6b89
EC2 Default User <ec2-user@ip-172-31-22-114.ec2.internal>
2021-02-24 19:34:46 +0000

    changed port

--- app.js
+++ app.js
@@ -51,7 +51,8 @@ app.post('/', (req, res) => {
     
 });
 
-const port = 8080;
+// const port = 8080;
+const port = 3000;
 
 const server = app.listen(port, () => console.log('Server started on port ' + port));
 
