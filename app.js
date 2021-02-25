37a732f4beaf2097ad1e9549b25894a272c65a01
EC2 Default User <ec2-user@ip-172-31-22-114.ec2.internal>
2021-02-25 17:57:26 +0000

    added personal site

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const ejs = require('ejs');
 const Nexmo = require('nexmo');
 const socketio = require('socket.io');
 
@@ -11,27 +10,21 @@ const nexmo = new Nexmo({
 
 const app = express();
 
-app.set('view engine', 'html');
-app.engine('html', ejs.renderFile);
+app.use(express.static(__dirname + '/public/welch_development'));
 
-app.use(express.static(__dirname + '/public'));
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + 'public/welch_development/index.html');
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.render('index');
-})
-
 app.post('/', (req, res) => {
-
     const number = req.body.number;
     const text = req.body.text;
     
     const numbers = [];
     numbers.push(number);
-    numbers.push(8034310547);
-    
         numbers.forEach(element => {
             nexmo.message.sendSms(
             '18772016887', '1' + element, text, { type: 'unicode' }, 
@@ -47,12 +40,10 @@ app.post('/', (req, res) => {
             }
         );
     })
-    
-    
 });
 
-// const port = 8080;
-const port = 3000;
+const port = 8080;
+// const port = 3000;
 
 const server = app.listen(port, () => console.log('Server started on port ' + port));
 
